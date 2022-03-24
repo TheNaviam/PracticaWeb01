3fae56e1d741d4a496bf21f65bd914fc269e7452
TheNaviam <ivan.aranda.martinez.11@gmail.com>
2022-03-24 15:32:44 +0100

    ASP

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using CurrículumASP.Services;
+using Microsoft.AspNetCore.Mvc;
+using PracticaWeb01.Models;
+using System.Diagnostics;
+using System.Net.Mail;
+
+namespace PracticaWeb01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Experiencia()
+        {
+            return View();
+        }
+
+        public IActionResult Estudios()
+        {
+            return View();
+        }
+
+        public IActionResult LicenciasCertificados()
+        {
+            return View();
+        }
+
+        public IActionResult Aptitudes()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        public IActionResult MisWebs()
+        {
+            ViewBag.Vname = new RepositoryOfWebs().GetWebs();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult metodoEmail(ClassEmail classEmail)
+        {
+            email(classEmail);
+            return View("Contacto");
+        }
+        private void email(ClassEmail classEmail)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(classEmail.email);
+
+            mail.From = new MailAddress("ggerardab@gmail.com");
+            mail.Subject = classEmail.asunto;
+            mail.Body = classEmail.mensaje;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("ggerardab@gmail.com", "2Barcelona$$");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Services/RepositoryOfWebs.cs
@@ -0,0 +1,53 @@
+﻿using PracticaWeb01.Models;
+
+namespace CurrículumASP.Services
+{
+    public class RepositoryOfWebs
+    {
+        public List<ClassWebs> GetWebs()
+        {
+            return new List<ClassWebs>()
+            {
+                new ClassWebs
+                {
+                    Title = "Proyecto Amazon",
+                    Description = "Web de Amazon",
+                    Link = "https://amazon.com",
+                    imageURL = "/images/amazon.png"
+                },
+
+                new ClassWebs
+                {
+                    Title = "Proyecto NY Times",
+                    Description = "Web de NY Times",
+                    Link = "https://nytimes.com",
+                    imageURL = "/images/nytimes.png"
+                },
+
+                new ClassWebs
+                {
+                    Title = "Proyecto Reddit",
+                    Description = "Web de Reddit",
+                    Link = "https://reddit.com",
+                    imageURL = "/images/reddit.png"
+                },
+
+                new ClassWebs
+                {
+                    Title = "Proyecto Steam",
+                    Description = "Web de Steam",
+                    Link = "https://store.steampowered.com",
+                    imageURL = "/images/steam.png"
+                },
+
+                new ClassWebs
+                {
+                    Title = "Proyecto Epic Games",
+                    Description = "Web de Epic Games",
+                    Link = "https://www.epicgames.com/",
+                    imageURL = "/images/epic.png"
+                },
+            };
+        }
+    }
+}
